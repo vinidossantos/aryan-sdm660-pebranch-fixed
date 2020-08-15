e8fcb86e38847138a096878d34ac31116e991608
vinidossantos <loguinsinho@gmail.com>
2020-08-14 21:00:44 -0300

    updated coral fp to aug

--- common_prop.mk
+++ common_prop.mk
@@ -1,5 +1,5 @@
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/flame/flame:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keyss"
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
