c972df6071d6c600ffc665c420760ca4a8e92a4a
vinidossantos <loguinsinho@gmail.com>
2020-11-14 13:01:17 -0300

    updated fp latest R

--- common_prop.mk
+++ common_prop.mk
@@ -1,5 +1,5 @@
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keyss"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
